2c1a04a3b2a8b8f46cdf9008409082e1b5632cd8
Subhajit Maji <42571438+subcoder167@users.noreply.github.com>
2021-02-13 23:24:11 +0530

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,6 @@ export default function Main(props) {
     const {products, onAdd} = props;
     return(
     <div className="content">
-        <h2>Products</h2>
         <div className="row">
             {products.map((product) => (
                 <Product key={product.id} product={product} onAdd={onAdd}></Product>
